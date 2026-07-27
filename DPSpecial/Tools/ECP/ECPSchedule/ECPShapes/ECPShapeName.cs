a5da180018a869abe35558832339b8ac9c912e6c
nvduc <duc.v.nguyen@prima-sol.com>
2026-07-27 22:45:56 +0700

    update

---
+++ DPSpecial/Tools/ECP/ECPSchedule/ECPShapes/ECPShapeName.cs
@@ -0,0 +1,27 @@
+﻿namespace DPSpecial.Tools.ECP.ECPSchedule.ECPShapes
+{
+    public class ECPShapeName
+    {
+        public const string EH0 = "EH0";
+        public const string EH1 = "EH1";
+        public const string EH3 = "EH3";
+        public const string EH4 = "EH4";
+        public const string EH5 = "EH5";
+        public const string EH20 = "EH20";
+        public const string EH29 = "EH29";
+        public const string EL0 = "EL0";
+        public const string EL1 = "EL1";
+        public const string EL3 = "EL3";
+        public const string EL4 = "EL4";
+        public const string EL5 = "EL5";
+        public const string EL20 = "EL20";
+        public const string EL29 = "EL29";
+        public const string ER0 = "ER0";
+        public const string ER1 = "ER1";
+        public const string ER3 = "ER3";
+        public const string ER4 = "ER4";
+        public const string ER5 = "ER5";
+        public const string ER20 = "ER20";
+        public const string ER29 = "ER29";
+    }
+}
